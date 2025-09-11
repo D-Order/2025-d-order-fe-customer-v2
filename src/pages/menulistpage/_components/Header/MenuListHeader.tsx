48b53ce4210293f82d89b5c311f9545d4f8b8282
taejun0 <ohtaetaejun@gmail.com>
2025-09-12 02:36:53 +0900

    [Design] #10 - 운영진 부르기 ui 작성

--- src/pages/menulistpage/_components/Header/MenuListHeader.tsx
+++ src/pages/menulistpage/_components/Header/MenuListHeader.tsx
@@ -1,6 +1,9 @@
 import * as S from './MenuListHeader.styled';
-
+import { useState } from 'react';
 import { MENULISTPAGE_CONSTANTS } from '../../_constants/menulistpageconstants';
+import alram from '../../../../assets/icons/alram.svg';
+import CallModal from '../modals/callmodal/CallModal';
+
 interface MenuListHeaderProps {
   onNavigate: () => void;
   onReceipt: () => void;
@@ -12,10 +15,17 @@ const MenuListHeader = ({
   onReceipt,
   cartCount,
 }: MenuListHeaderProps) => {
+  const [ismodalOpen, setIsModalOpen] = useState(false);
+  const tableNum = Number(localStorage.getItem('tableNum'));
+
   return (
     <S.Wrapper>
       <S.Logo src={MENULISTPAGE_CONSTANTS.HEADER.IMAGE.LOGOPNG} />
       <S.Icons>
+        <S.Hochul onClick={() => setIsModalOpen(true)}>
+          직원 호출
+          <S.Icon src={alram} />
+        </S.Hochul>
         <S.Icon
           src={MENULISTPAGE_CONSTANTS.HEADER.IMAGE.RECEIPT}
           onClick={onReceipt}
@@ -28,6 +38,9 @@ const MenuListHeader = ({
           {cartCount > 0 && <S.Badge />}
         </S.IconWrap>
       </S.Icons>
+      {ismodalOpen && (
+        <CallModal onClose={() => setIsModalOpen(false)} tableNum={tableNum} />
+      )}
     </S.Wrapper>
   );
 };
